a3216c56e28d24f8dd717e17e03aa97c9b8a018a
dsurush <DavlatzodaS@HUMO.local>
2020-04-27 20:10:39 +0500

    fixed: add middleware to other routers

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -18,8 +18,8 @@ func (server *MainServer) InitRoutes() {
 	server.router.GET("/api/client/:phone", logger.Logger(`Get client: `)(jwt.JWT(reflect.TypeOf((*token.Payload)(nil)).Elem(), []byte(`surush`))((authorized.Authorized([]string{`admin`}, jwt.FromContext)(server.GetClientInfoByPhoneNumberHandler)))))
 	server.router.GET("/api/clients", logger.Logger(`Get all clients By Page and Rows`)(jwt.JWT(reflect.TypeOf((*token.Payload)(nil)).Elem(), []byte(`surush`))((authorized.Authorized([]string{`admin`}, jwt.FromContext)(server.GetClientsInfoByFilterHandler)))))
 	server.router.GET("/api/vendors", logger.Logger(`Get vendors `)(jwt.JWT(reflect.TypeOf((*token.Payload)(nil)).Elem(), []byte(`surush`))((authorized.Authorized([]string{`admin`}, jwt.FromContext)(server.GetVendorCategoryByPageSizeHandler)))))
-	server.router.GET(`/api/transactions`, server.GetViewTransactionsHandler)
-	server.router.GET(`/api/reports`, server.GetViewReportsHandler)
+	server.router.GET(`/api/transactions`, logger.Logger(`Get transactions `)(jwt.JWT(reflect.TypeOf((*token.Payload)(nil)).Elem(), []byte(`surush`))((authorized.Authorized([]string{`admin`}, jwt.FromContext)(server.GetViewTransactionsHandler)))))
+	server.router.GET(`/api/reports`, logger.Logger(`Get reports `)(jwt.JWT(reflect.TypeOf((*token.Payload)(nil)).Elem(), []byte(`surush`))((authorized.Authorized([]string{`admin`}, jwt.FromContext)(server.GetViewReportsHandler)))))
 	panic(http.ListenAndServe("127.0.0.1:8080", server))
 }
 
