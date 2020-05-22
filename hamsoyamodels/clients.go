76dff28362ed477c1f20cf04e026eafa00b5adb9
dsurush <DavlatzodaS@HUMO.local>
2020-05-22 23:11:20 +0500

    add hamsoya client struct and method tablename

---
+++ hamsoyamodels/clients.go
@@ -0,0 +1,19 @@
+package hamsoyamodels
+
+import "time"
+
+type HamsoyaClient struct {
+	Id         int64     `xml:"id"`
+	PhoneNum   string    `xml:"phone_num"`
+	AgentId    int64     `xml:"agent_id"`
+	TypeId     int64     `xml:"type_id"`
+	IsActive   bool      `xml:"is_active"`
+	CreateDate time.Time `xml:"create_date"`
+	CloseDate  time.Time `xml:"close_date"`
+	Identify   bool      `xml:"identify"`
+	Name       string    `xml:"name"`
+}
+
+func (*HamsoyaClient) TableName() string {
+	return "clients"
+}
